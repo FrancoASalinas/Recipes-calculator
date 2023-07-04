8b36c846fd944abcb09492407acf3f4c3172dd11
FrancoSMiño <frankow222@gmail.com>
2023-07-04 14:19:27 -0300

    restarted project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
+import { Box } from '@mui/material';
+import Recipe from './modules/Recipe';
+import VerticalLinearStepper from './modules/VerticalLinearStepper';
 import { useState } from 'react';
-import './App.scss';
-import OriginalRecipe from './modules/OriginalRecipe';
-import Header from './modules/Header';
-import Main from './modules/Main';
+import Conversion from './modules/Conversion';
+import NewRecipe from './modules/NewRecipe';
 
-export default function App() {
+function App() {
+  const [activeStep, setActiveStep] = useState(0);
+  const [originalRecipe, setOriginalRecipe] = useState('');
+
+  const handleNext = () => {
+    setActiveStep((prev) => prev + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <Box
+      bgcolor="#000"
+      minWidth="100vw"
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        bgcolor="#333"
+        width="200px"
+        height="200px"
+        borderRadius="20px"
+        minHeight="28rem"
+        minWidth="80%"
+        padding="1rem"
+        color="primary.main"
+        display="grid"
+        alignItems="center"
+        gridTemplateColumns="1fr 1fr"
+      >
+        <VerticalLinearStepper
+          onNext={handleNext}
+          onBack={handleBack}
+          activeStep={activeStep}
+        />
+        <Box display="flex" alignItems="center" flexDirection="column">
+          {activeStep === 0 && <Recipe />}
+          {activeStep === 1 && <Conversion />}
+          {activeStep === 2 && <NewRecipe />}
+        </Box>
+      </Box>
+    </Box>
   );
 }
+
+export default App;
