823a0c93ed32e2b4591a8d7304d71d3d7767baf2
FrancoSMiño <frankow222@gmail.com>
2023-10-01 20:40:54 -0300

    Styled Error Page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className='bg-mimipink min-h-screen'>
+    <div className='bg-mimipink min-h-screen grid grid-rows-[70px_1fr]'>
       <Header />
-      <main className='pt-3 font-incsans max-w-4xl text-richblack mx-auto'>
+      <main className='pt-3 font-incsans max-w-4xl self-center justify-self-center text-richblack mx-auto'>
       <Outlet />
       </main>
     </div>
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function ErrorPage() {
-  return <>404 page not found</>;
+  return (
+    <div className='flex flex-col justify-between gap-5 items-center'>
+      <div className='space-y-1 text-center'>
+        <h1 className='text-6xl font-croissant '>Error 404</h1>
+        <h2 className='text-4xl font-croissant'>Page not found</h2>
+      </div>
+      <p className='text-3xl text-center'>
+        The page you are looking for does not exist, please check the URL or:
+      </p>
+      <div className='flex-col mt-3 flex group h-fit w-fit'>
+        <Link to='/' className='text-2xl'>
+          Go to Homepage
+        </Link>
+        <div className='h-[2px] w-0 group-hover:w-full bg-nn transition-all'></div>
+      </div>
+    </div>
+  );
 }
 
 export default ErrorPage;
