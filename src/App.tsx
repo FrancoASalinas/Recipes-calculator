0ed674e8fc653f821074e12bb41f6d8fbf7e0779
FrancoSMiño <frankow222@gmail.com>
2023-10-03 22:59:07 -0300

    Added code splitting due to excessive bundle size.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <div className='bg-mimipink min-h-screen grid grid-rows-[70px_1fr]'>
       <Header />
       <main className='pt-3 font-incsans max-w-4xl self-center justify-self-center text-richblack mx-auto'>
-      <Outlet />
+        <Suspense fallback={<p className='text-3xl'>Loading...</p>}>
+          <Outlet />
+        </Suspense>
       </main>
     </div>
   );
--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,14 +1,16 @@
 import { Navigate, Route } from 'react-router-dom';
 import App from '../App';
-import NewRecipe from '../pages/NewRecipe';
 import ErrorPage from '../pages/ErrorPage';
 import Homepage from '../pages/Homepage';
+import { lazy } from 'react';
+const NewRecipe = lazy(() => import('../pages/NewRecipe'));
 
 export const routes = (
+
   <Route path='/' element={<App />}>
-    <Route index element={<Homepage />} />
-    <Route path='new' element={<NewRecipe />} />
-    <Route path='404' element={<ErrorPage />} />
-    <Route path='*' element={<Navigate to='404' />} />
+        <Route index element={<Homepage />} />
+        <Route path='new' element={<NewRecipe />} />
+        <Route path='404' element={<ErrorPage />} />
+        <Route path='*' element={<Navigate to='404' />} />
   </Route>
 );
